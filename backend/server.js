9724d17ac7c620805726642adf7845bc8b20c540
Arya Bhavsar <aryab2914@gmail.com>
2026-01-22 22:30:12 -0500

    Setup the app

--- backend/server.js
+++ backend/server.js
@@ -11,13 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Sample route
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });

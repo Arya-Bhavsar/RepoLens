ca3c44fd70556bbe410d40a389b8afc21f980a3d
Arya Bhavsar <aryab2914@gmail.com>
2026-01-24 11:31:39 -0500

    Designed the login page

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Login() {
     const [email, setEmail] = useState('');
@@ -10,24 +11,32 @@ export default function Login() {
     }
     
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)} 
-                    required
-                />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)} 
-                    required
-                />
-                <button type="submit">Login</button>
-            </form>
+        <div className="min-h-screen flex justify-center items-center bg-gray-100 dark:bg-zinc-900">
+            <div className="bg-white dark:bg-zinc-800 p-12 rounded-lg shadow-lg w-full max-w-md">
+                <div className="text-3xl font-bold text-gray-800 dark:text-gray-100">Log in</div>
+                <p className="text-zinc-400 mb-12">or{" "}
+                    <Link to="/signup" className="text-blue-600 dark:text-blue-500 hover:underline">create an account</Link>
+                </p>
+                <form onSubmit={handleSubmit} className="space-y-7">
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)} 
+                        required
+                        className="w-full px-4 py-2 border border-gray-300 dark:border-zinc-600 bg-gray-50 dark:bg-zinc-700 text-gray-900 dark:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)} 
+                        required
+                        className="w-full px-4 py-2 border border-gray-300 dark:border-zinc-600 bg-gray-50 dark:bg-zinc-700 text-gray-900 dark:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                    <button type="submit" className="mt-4 cursor-pointer w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold px-4 py-2 rounded-md transition-colors duration-200">Log in</button>
+                </form>
+            </div>
         </div>
     )
 }

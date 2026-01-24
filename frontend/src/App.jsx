c2120c6f6ebbe6285a455acdc0750f2004c34f0d
Arya Bhavsar <aryab2914@gmail.com>
2026-01-24 11:42:23 -0500

    Added an empty dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   )

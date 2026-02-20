bb47fb7b36642dabcf9ebbfe9fcf6ccce9845978
Arya Bhavsar <aryab2914@gmail.com>
2026-02-20 11:24:48 -0500

    Fixed light mode colors

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
     const { currentUser } = useContext(UserContext);
 
     return (
-        <header className="flex justify-between items-center px-6 py-4 bg-white dark:bg-zinc-800 border-b border-zinc-200 dark:border-zinc-700">
+        <header className="flex justify-between items-center px-6 py-4 bg-gray-100 dark:bg-zinc-800 border-b border-zinc-200 dark:border-zinc-700">
             {/* Left corner: Logo and App Name */}
             <div className="flex items-center">
                 <span className="text-2xl font-bold text-gray-800 dark:text-gray-100">Repo</span>
@@ -21,7 +21,7 @@ export default function Header() {
                 {/* User Profile Avatar */}
                 <div className="flex items-center gap-2">
                     <Avatar>
-                        <Avatar.Fallback className="bg-gray-100! dark:bg-zinc-900! border border-zinc-200! dark:border-zinc-700!">
+                        <Avatar.Fallback className="bg-white! dark:bg-zinc-900! border border-zinc-200! dark:border-zinc-700!">
                             {currentUser.first_name.charAt(0)}{currentUser.last_name.charAt(0)}
                         </Avatar.Fallback>
                     </Avatar>

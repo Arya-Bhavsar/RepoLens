2a7c25636cecda6a725c6885e14105779fec6bb4
Arya Bhavsar <122700593+Arya-Bhavsar@users.noreply.github.com>
2026-03-14 18:50:58 -0400

    added blue bubble for user prompt

--- frontend/src/components/ChatWindow.jsx
+++ frontend/src/components/ChatWindow.jsx
@@ -3,16 +3,25 @@ import { Input } from '@heroui/react';
 
 export default function ChatWindow() {
     const [prompt, setPrompt] = useState("");
+    const [messages, setMessages] = useState([]);
 
     const handleKeyDown = (e) => {
         if (e.key === "Enter" && prompt.trim() !== "") {
-            // Code here
+            setMessages(prev => [...prev, prompt])
+            setPrompt("");
         }
     };
 
     return (
         <div className="flex flex-col justify-between h-full bg-white! dark:bg-zinc-950! border-l border-zinc-200 dark:border-zinc-700 overflow-hidden">
             {/* Chat Bubbles */}
+            <div className="flex flex-col p-4 gap-4">
+                {messages.map((msg, i) => (
+                    <div key={i} className="self-end max-w-[80%] px-3 py-2 text-white text-sm wrap-break-word bg-blue-500 rounded-tl-xl rounded-tr-xl rounded-bl-xl">
+                        {msg}
+                    </div>
+                ))}
+            </div>
 
             {/* Text Input */}
             <div className="flex flex-col mt-auto px-6 pb-6 w-full p-4 justify-center">
